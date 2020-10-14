f8aa19432d10461daa9731911fba06104c900e70
VDA-001 <vdavishu@gmail.com>
2020-10-14 11:13:55 +0530

    Order-app

--- api/order/urls.py
+++ api/order/urls.py
@@ -13,16 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls.static import static
+from rest_framework import routers
 from django.urls import path, include
-from rest_framework.authtoken import views
 from . import views
 
+router = routers.DefaultRouter()
+router.register(r'', views.OrderViewSet)
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('category/',include('api.category.urls')),
-    path('product/',include('api.product.urls')),
-    path('user/',include('api.user.urls')),
-    path('order/',include('api.order.urls'))
-    #path('api-token-auth/',views.obtain_auth_token, name='api_token_auth')
+    path('add/<str:id>/<str:token>/',views.add,name='order.add'),
+    path('', include(router.urls))
 ]
